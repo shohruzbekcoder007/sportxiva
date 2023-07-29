1d14c54bf39b645e8548b060346937c16f879c2c
JaMeS-18-18 <chitak1818@gmail.com>
2023-07-29 17:36:30 +0500

    news added

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -15,7 +15,6 @@ export default function Main() {
   return (
     <>
         {/* <Header/> */}
-        <Header/>
         <VideoWelcome/>
         <Welcome/>
         <FestInfo/>
@@ -26,7 +25,6 @@ export default function Main() {
         <Walks/>
         <FolkGames/>
         {/* <Footer/> */}
-        <Footer/>
     </>
   ) 
 }
