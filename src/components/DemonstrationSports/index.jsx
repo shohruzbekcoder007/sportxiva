c8fd61295c3391aefd8d7db436ed92f374a464a8
shohruzbekcoder <shohruzbekcoder007@gamil.com>
2023-07-29 08:26:30 +0500

    animation chempionat 8:26

--- src/components/DemonstrationSports/index.jsx
+++ src/components/DemonstrationSports/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MainWrapper, Title } from '../../global_styles/styles'
-import { DemonstrationSportsContainer, DemonstrationSportsList, DemonstrationSportsListItem, DemonstrationSportsTitle } from './styles'
+import { DemonstrationSportsContainer, DemonstrationSportsImgWrapper, DemonstrationSportsList, DemonstrationSportsListItem, DemonstrationSportsTitle } from './styles'
 
 export default function DemonstrationSports() {
   return (
@@ -10,25 +10,40 @@ export default function DemonstrationSports() {
         </DemonstrationSportsTitle>
         <DemonstrationSportsContainer>
             <DemonstrationSportsList>
-                <DemonstrationSportsListItem>
+                <DemonstrationSportsListItem className="animation-info">
                     <span>01</span>
                     <h5>Bel olish va uch kurash</h5>
+                    <DemonstrationSportsImgWrapper className="animation-img">
+                        <img src={require('../../imgs/ozbek-jang-sanati-0-0-0-0-1629875866.png')} alt="ozbek-jang-sanati" />
+                    </DemonstrationSportsImgWrapper>
                 </DemonstrationSportsListItem>
-                <DemonstrationSportsListItem>
+                <DemonstrationSportsListItem className="animation-info">
                     <span>02</span>
                     <h5>Qush ovi</h5>
+                    <DemonstrationSportsImgWrapper className="animation-img">
+                        <img src={require('../../imgs/mas_1540.png')} alt="ozbek-jang-sanati" />
+                    </DemonstrationSportsImgWrapper>
                 </DemonstrationSportsListItem>
-                <DemonstrationSportsListItem>
+                <DemonstrationSportsListItem className="animation-info">
                     <span>03</span>
                     <h5>Ko’pkari</h5>
+                    <DemonstrationSportsImgWrapper className="animation-img">
+                        <img src={require('../../imgs/IMG_20211117_235855_401.png')} alt="ozbek-jang-sanati" />
+                    </DemonstrationSportsImgWrapper>
                 </DemonstrationSportsListItem>
-                <DemonstrationSportsListItem>
+                <DemonstrationSportsListItem className="animation-info">
                     <span>04</span>
                     <h5>Kamondan o’q otish</h5>
+                    <DemonstrationSportsImgWrapper className="animation-img">
+                    <img src={require('../../imgs/IMG_20211117_235855_401.png')} alt="ozbek-jang-sanati" />
+                    </DemonstrationSportsImgWrapper>
                 </DemonstrationSportsListItem>
-                <DemonstrationSportsListItem>
+                <DemonstrationSportsListItem className="animation-info">
                     <span>05</span>
                     <h5>Turon yakka kurashi</h5>
+                    <DemonstrationSportsImgWrapper className="animation-img">
+                        <img src={require('../../imgs/ozbek-jang-sanati-0-0-0-0-1629875866.png')} alt="ozbek-jang-sanati" />
+                    </DemonstrationSportsImgWrapper>
                 </DemonstrationSportsListItem>
             </DemonstrationSportsList>
         </DemonstrationSportsContainer>
--- src/components/DemonstrationSports/styles.js
+++ src/components/DemonstrationSports/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const DemonstrationSportsTitle = styled.div`
     padding: 150px 0;
@@ -15,6 +15,7 @@ export const DemonstrationSportsList = styled.ul`
 
 export const DemonstrationSportsListItem = styled.li`
     width: 100%;
+    position: relative;
     span {
         display: inline-block;
         color: ${props => props.theme.text.font};
@@ -26,7 +27,6 @@ export const DemonstrationSportsListItem = styled.li`
     }
     h5 {
         color: ${props => props.theme.text.font};
-        font-family: Nunito Sans;
         font-size: 48px;
         font-style: normal;
         font-weight: 800;
@@ -34,5 +34,22 @@ export const DemonstrationSportsListItem = styled.li`
         padding-bottom: 55px;
     }
     border-bottom: 1px solid ${props => props.theme.color.color1};
-    margin-bottom: 150px;
+    margin-bottom: 60px;
+`
+
+export const DemonstrationSportsImgWrapper = styled.div`
+    width: 300px;
+    height: 340px;
+    flex-shrink: 0;
+    background-color: red;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(60%) rotate(-45deg);
+    opacity: -1;
+    z-index: -1;
+    img {
+        width: 100%;
+        height: 100%;
+    }
 `
